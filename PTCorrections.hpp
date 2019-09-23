f8100eb098a731826139f5e27c9b18a332d21346
Enigma7761 <47766594+Enigma7761@users.noreply.github.com>
2019-09-22 22:47:26 -0700

    Descriptions for PTCorrections Methods

--- PTCorrections.hpp
+++ PTCorrections.hpp
@@ -24,6 +24,7 @@ void clearSignalArray(){
 void readData(){
     //reads from the file into the signals array
     for(int i = 0; i < BUFFER; i++){
+        //if not end of file, read what's next in the file into the array, otherwise just put 0
         if(!feof(fin))
             fscanf(fin,"%d\n",&signals[i]);
         else
@@ -36,6 +37,7 @@ void markSignals(){
     //marks signals to be used as an anchor
     int anchor;
     bool found = false;
+    //finds the anchor
     for(int i = 0; i < BUFFER; i++){
         if(signals[i] == 1){
             markedSignals[i] = 1;
@@ -44,14 +46,21 @@ void markSignals(){
         }
     }
     while(anchor + (int)(averageInterval*0.2) < BUFFER){
+    //while the anchor and the range of search does not reach the end of the buffer
         for(int i = 0; i < (int)(averageInterval * 0.2); i++){
+            //limits the search to a range of +- averageInterval*0.2. If there is no signal found, then it just marks at the predicted location of
+            //anchor + averageInterval. Otherwise mark the closest signal to anchor+averageInterval and set that as the new anchor
             if(signals[anchor + i + averageInterval] == 1){
+                //checks in signals at the anchor + the averageInterval - i 
+                //if there is a signal, add this into markedSignals, set anchor to this index, and break out of the for loop
                 markedSignals[anchor + i + averageInterval] = 1;
                 anchor += i + averageInterval;
                 found = true;
                 break;
             }
             else if(signals[anchor + averageInterval - i] == 1){
+                //checks in signals at the anchor + the averageInterval - i
+                //if there is a signal, add this into markedSignals, set anchor to this index, and break out of the for loop
                 markedSignals[anchor + averageInterval - i] = 1;
                 anchor += averageInterval - i;
                 break;
@@ -59,14 +68,17 @@ void markSignals(){
             }
         }
         if(!found == true){
+            //if there was no signal in this range, just put one where it would be predicted to be based on the average (for missing signals)
             markedSignals[anchor + averageInterval] = 1;
             anchor += averageInterval;
         }
+        //resets false
         found = false;
     }
 }
 
 void writeToFile(){
+    //for now it just writes the markedSignals. Eventually it will write from signals whenever signals is corrected even further
     for(int i = 0; i < BUFFER; i++){
         fprintf(fout,"%d\n",markedSignals[i]);
     }
@@ -81,7 +93,9 @@ void writeToTerminal(){
 void calcAverageInterval(){
     //calculates the average rr interval in the array
     averageInterval = 0;
+    //first index of signal
     int first = -1;
+    //last index of signal
     int last = -1;
     for(int i = 0; i < BUFFER; i++){
         if(signals[i] == 1){
@@ -94,10 +108,12 @@ void calcAverageInterval(){
             numPeaks++;
         }
     }
+    //total range that still has peaks/number of peaks in that time
     averageInterval = (last - first) / numPeaks;
 }
 
 void PTCorrections(){
+    //skips the initialization
     for(int i = 0; i < SKIP; i++)
         fscanf(fin,"%*d\n");
     while(!feof(fin)){
