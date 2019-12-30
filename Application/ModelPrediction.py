2114276aca83bce95921e05c25a9037066cea57d
Enigma7761 <47766594+Enigma7761@users.noreply.github.com>
2019-12-29 21:20:55 -0800

    Final Update to modelprediction

--- Application/ModelPrediction.py
+++ Application/ModelPrediction.py
@@ -28,20 +28,20 @@ Model.load_weights("Model.h5")
 ecg = []
 ecg_from_file(ecg, os.path.join('..','ECG_Data','T21.ascii'))
 signal = np.zeros((len(ecg)))
-var = []
+std = []
 
 with tf.device("/device:CPU:0"):
     for i in range(0,len(ecg) - interval_length,step):
-        if i % 10000 == 0:
-            print(i)
+        #if i % 10000 == 0:
+        #    print(i)
         temp = np.asarray(ecg[i:i+interval_length])*100
         temp = Model.predict(temp.reshape(1,interval_length, 1))
-        var.extend(repeat(np.var(temp),step))
-        if np.var(temp) > 0.001:
-            continue
-        else:
-            temp -= np.average(temp)
-            signal[i:i + interval_length] += temp.reshape(400,)
+        std.extend(repeat(np.std(temp*10),step))
+        #if np.var(temp) > 0.002:
+            #continue
+        #else:
+        temp -= np.average(temp)
+        signal[i:i + interval_length] += temp.reshape(400,)
 
 signal /= (interval_length/step)
 
@@ -55,6 +55,6 @@ fig, axs = plt.subplots(2, sharex = True)
 axs[0].plot(range(len(ecg)), ecg)
 axs[0].plot(range(len(signal)), signal)
 axs[0].axis([0,6000,-0.5,1])
-axs[1].plot(range(len(var)), var)
+axs[1].plot(range(len(std)), std)
 axs[1].axis([0,6000,-0.5,1])
 plt.show()
