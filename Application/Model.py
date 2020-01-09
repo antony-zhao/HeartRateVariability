73fc4db7835b22fd07e66d2048d7f84fae391e64
Enigma7761 <47766594+Enigma7761@users.noreply.github.com>
2020-01-08 21:08:45 -0800

    Optimizing Model

--- Application/Model.py
+++ Application/Model.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv1D, Dense, Dropout, Flatten, MaxPooling1D, Activation
+from tensorflow.keras.layers import Conv1D, Dense, Dropout, Flatten, MaxPooling1D, Activation, GRU
 import tensorflow as tf
 import re
 from Methods import *
@@ -18,35 +18,33 @@ if len(physical_devices) > 0:
 
 interval_length = 400
 
-for x in open(os.path.join('..','Training','Ecg1.txt')):
-    ecg1.append(float(re.findall('([-0-9.]+)', x)[-1]))
-
-for x in open(os.path.join('..','Training','Sig1.txt')):
-    s1.append(float(re.findall('([-0-9.]+)', x)[-1]))
-
-x_train, y_train = random_sampling(ecg1, s1, 5000, interval_length)
-x_test, y_test = random_sampling(ecg1, s1, 100, interval_length)
-
-date = datetime.now().strftime("%Y%m%d-%H%M%S")
-logdir = os.path.join('Training', date)
-
-
 Model = Sequential()
-Model.add(Conv1D(input_shape = (interval_length,1), filters = 20, kernel_size = 4,strides = 2, activation = 'relu'))
+Model.add(Conv1D(input_shape = (interval_length,1), filters = 10, kernel_size = 4,strides = 2, activation = 'relu'))
 Model.add(MaxPooling1D())
-Model.add(Conv1D(50, kernel_size = 8, strides = 2, activation = 'relu'))
+Model.add(Conv1D(20, kernel_size = 8, strides = 2, activation = 'relu'))
 Model.add(MaxPooling1D())
 Model.add(Flatten())
-Model.add(Dense(600))
+Model.add(Dense(200))
 Model.add(Activation('relu'))
 Model.add(Dropout(0.2))
 Model.add(Dense(interval_length))
 Model.add(Activation('softmax'))
-modelFile = 'Model.h5'
 
-try:
+def load_model(modelFile):
     Model.load_weights(modelFile)
-except:
+
+def train_model(epochs, modelFile):
+    for x in open(os.path.join('..', 'Training', 'Ecg1.txt')):
+        ecg1.append(float(re.findall('([-0-9.]+)', x)[-1]))
+
+    for x in open(os.path.join('..', 'Training', 'Sig1.txt')):
+        s1.append(float(re.findall('([-0-9.]+)', x)[-1]))
+
+    x_train, y_train = random_sampling(ecg1, s1, 5000, interval_length)
+
+    date = datetime.now().strftime("%Y%m%d-%H%M%S")
+    logdir = os.path.join('Training', date)
+
     Model.compile(optimizer = 'Adadelta', loss = 'categorical_crossentropy', metrics = ['accuracy'])
     file_writer = tf.summary.create_file_writer(logdir + "/metrics")
     file_writer.set_as_default()
@@ -67,8 +65,7 @@ except:
     lr_callback = keras.callbacks.LearningRateScheduler(lr_schedule)
     tsbrd = tf.keras.callbacks.TensorBoard(log_dir = logdir)
 
-    #with tf.device("/device:CPU:0"):
-    hist = Model.fit(x_train, y_train, batch_size = 64, validation_data = (x_test, y_test), callbacks = [tsbrd, lr_callback],epochs = 50, verbose = 2)
+    Model.fit(x_train, y_train, batch_size = 64, callbacks = [tsbrd, lr_callback],epochs = epochs, verbose = 2)
 
     Model.save(modelFile)
 
--- Application/ModelPrediction.py
+++ Application/ModelPrediction.py
@@ -2,56 +2,51 @@ from matplotlib import pyplot as plt
 import numpy as np
 import os
 from Methods import *
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv1D, Dense, Dropout, Flatten, MaxPooling1D, Activation
 import tensorflow as tf
-from itertools import repeat
-from Model import Model
+from Model import Model, train_model, load_model, interval_length
 import time
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-interval_length = 400
-step = 100
+step = interval_length//2
+epochs = 100
+
+modelFile = 'Model.h5'
 
 ecg = []
 ecg_from_file(ecg, os.path.join('..','ECG_Data','T21.ascii'))
 signal = np.zeros((len(ecg)))
-std = []
 
-start = time.time()
+try:
+    load_model(modelFile)
+except:
+    train_start = time.time()
+    train_model(epochs, modelFile)
+    print(time.time() - train_start)
+    load_model(modelFile)
+finally:
+    start = time.time()
 
-with tf.device("/device:CPU:0"):
     for i in range(0,len(ecg) - interval_length,step):
-        #if i % 10000 == 0:
-        #    print(i)
         temp = np.asarray(ecg[i:i+interval_length])*100
         temp = Model.predict(temp.reshape(1,interval_length, 1))
-        std.extend(repeat(np.std(temp*10),step))
-        #if np.var(temp) > 0.002:
-            #continue
-        #else:
         temp -= np.average(temp)
         signal[i:i + interval_length] += temp.reshape(400,)
-end = time.time()
+    end = time.time()
 
-print('elapsed time: ' + str(end-start))
+    print('elapsed time: ' + str(end-start))
 
-signal /= (interval_length/step)
+    signal /= (interval_length/step)
 
-f = open(os.path.join('..','Signal','SignalPy.txt'),'w')
-for i in signal:
-    f.write(str(i) + '\n')
-f.close()
+    f = open(os.path.join('..','Signal','SignalPy.txt'),'w')
+    for i in signal:
+        f.write(str(i) + '\n')
+    f.close()
 
-#fig, axs = plt.subplots(2, sharex = True)
+    plt.plot(range(len(ecg)), ecg)
+    plt.plot(range(len(signal)), signal)
+    plt.axis([64000,70000,-0.5,1])
+    plt.show()
 
-plt.plot(range(len(ecg)), ecg)
-plt.plot(range(len(signal)), signal)
-plt.axis([64000,70000,-0.5,1])
-#axs[1].plot(range(len(std)), std)
-#axs[1].axis([0,6000,-0.5,1])
-plt.show()
-
-del Model
-tf.keras.backend.clear_session()
+    del Model
+    tf.keras.backend.clear_session()
