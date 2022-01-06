0ddbd3ef61818c7c344bcad60c996fa222308309
Antony Zhao <ayzhao7761@berkeley.edu>
2022-01-06 04:33:06 -0800

    Documentation for post_processing.py

--- Application/post_processing.py
+++ Application/post_processing.py
@@ -14,24 +14,33 @@ import multiprocessing as mp
 import json
 import tqdm
 from functools import partial
-from config import interval_length, step, stack, scale_down, datapoints, \
-    lines_per_file, T, fs, low_cutoff, high_cutoff, nyq, order
+from config import interval_length
 
-pbar = tqdm.tqdm
+"""
+After the model_prediction.py program splits the data into multiple files, this
+program uses multiprocessing to process multiple files in parallel and rejoins the
+final data into a single excel sheet.
+"""
 
 
 def process_file(filenames, filename):
+    """
+    Processes a single file and returns
+    Args:
+        filenames (list): A list of all the file names, used
+            for tracking which files are done processing
+        filename (str): The file to be processed
+    """
     global interval_length
-    first = True
+    avg_interval_length = interval_length
+    first = True  # For the first signal of the file
     last_few = deque(maxlen=8)
-    dist = 0
-    reset = True
+    dist = 0  # Distance from the last peak
+    reset = True  # If the gap is too wide and it needs to reset the distance
     file = open(os.path.join('..', 'Signal', filename), 'r+')
-    file_loc = file.tell()
-    temp_line = file.readline()
-    file.seek(file_loc)
-    line_size = len(temp_line.encode('utf-8'))
-    lines = []
+    lines = []  # Lines that will be later written to the excel sheet,
+    # contains tuples of the datetime, as well as the distance, which is an empty string if it's the first
+    # signal of the file, or if the gap was too wide
     for i, line in enumerate(file):
         dist += 1
         temp = re.findall('([-0-9.]+)', line)
@@ -41,52 +50,68 @@ def process_file(filenames, filename):
 
         signal = int(temp[-1])
         if signal == 1:
-            if dist > 0.8 * interval_length or dist == 1 or first:  # This would mean that the signal is correct
+            if dist > 0.8 * avg_interval_length or dist == 1 or first:  # This would mean that the signal is correct
                 if dist == 1:  # For the areas where the signal is marked multiple times
                     dist = 0
                 else:
-                    if dist > 1.7 * interval_length:  # This indicates that the gap is too large
-                        # Write the datetime, ecg signal, but no RR interval
-                        lines.append((date, ''))
-                    elif 1.2 * interval_length < dist < 1.7 * interval_length:  # For when one beat is missed and the next one is also wrong
+                    if dist > 1.7 * avg_interval_length:  # This indicates that the gap is too large
+                        lines.append((date, ''))  #
+                    elif 1.2 * avg_interval_length < dist < 1.7 * avg_interval_length:  # For when one beat is missed and the
+                        # next one is also wrong
                         continue
                     else:
-                        # Write the datetime, ecg signal, RR interval (or blank if reset is True)
-                        lines.append((date, '' if reset else dist / 4))
+                        lines.append((date, '' if reset else dist / 4))  # dist/4 because ours is sampled as 4
+                        # datapoints per milisecond
                         if reset:
                             reset = False
 
             else:
                 reset = True
-            if 1.2 * interval_length > dist > 0.8 * interval_length:
-                last_few.append(dist)  # add the distance to the running average
+            if 1.2 * avg_interval_length > dist > 0.8 * avg_interval_length:
+                last_few.append(dist)  # Add the distance to the running average
             dist = 0  # Reset distance between last and current signal
             if first:
                 first = False  # handling first signal
             if len(last_few) == 8:
-                interval_length = np.mean(last_few)  # running average of rr interval
+                avg_interval_length = np.mean(last_few)  # running average of rr interval
     print("{}/{} file has been completed".format(filenames.index(filename) + 1, len(filenames)))
     return lines
 
 
-def write_to_excel(lines, sheet1, row, wb):
-    interval_format = wb.add_format({'num_format': '#.00'})
-    date_format = wb.add_format({'num_format': 'm/d/y'})
-    time_format = wb.add_format({'num_format': 'hh:mm:ss.000'})
-    diff_format = wb.add_format({'num_format': '0.00'})
+def write_to_excel(lines, sheet, row, formats):
+    """
+    Writes the lines to the sheet, as well as calculating some useful data which are
+    related to heart rate variability.
+
+    Args:
+        lines (list): A list containing the datetimes of when the signal occured as well as
+            the distance between it and the previous signal
+        sheet (Workbook.worksheet): The excel sheet which the data will be written to
+        row (int): The row to write the data to (also be returned for the next call as the
+            function is called once for the data of each file
+        formats (tuple): A tuple containing the different formats to write to cells.
+
+    """
+    interval_format = formats[0]
+    date_format = formats[1]
+    time_format = formats[2]
+    diff_format = formats[3]
     prev_value = ''
     start_time = lines[0][0]
-    intervals = []
+    intervals = []  # Tracks the last RR-intervals for a certain duration, which we get data from after
     for line in lines:
-        sheet1.write_datetime(row, 0, line[0], date_format)
-        sheet1.write_datetime(row, 1, line[0], time_format)
-        sheet1.write(row, 4, row)
-        sheet1.write(row, 5, line[1], interval_format)
-        sheet1.write(row, 6, (line[1] - prev_value) ** 2 if not prev_value == '' and not line[1] == '' else '',
-                     diff_format)
-        if line[0] - start_time >= datetime.timedelta(minutes=2):
-            sheet1.write(row, 7, np.mean(intervals), interval_format)
-            sheet1.write(row, 8, np.std(intervals), interval_format)
+        sheet.write_datetime(row, 0, line[0], date_format)  # Splits the datetime value
+        sheet.write_datetime(row, 1, line[0], time_format)
+        sheet.write(row, 4, row)  # Just the number of the signal (shouldn't be different from the row
+        # in our version but can be changed)
+        sheet.write(row, 5, line[1], interval_format)  # The number of ms between peaks
+        sheet.write(row, 6, (line[1] - prev_value) ** 2 if not prev_value == '' and not line[1] == '' else '',
+                    diff_format)  # The squared difference between the RR-interval of this and
+        # the previous data (if possible)
+        if line[0] - start_time >= datetime.timedelta(minutes=2):  # The average and standard deviation in the
+            # RR-interval in 2 minutes
+            sheet.write(row, 7, np.mean(intervals), interval_format)
+            sheet.write(row, 8, np.std(intervals), interval_format)
             start_time = line[0]
             intervals.clear()
         if line[1] != '':
@@ -97,9 +122,10 @@ def write_to_excel(lines, sheet1, row, wb):
 
 
 def main():
+    # Handles selecting the file and selecting a place/name for the new file
     root = tk.Tk()
     currdir = os.getcwd()
-    root.filename = filedialog.askopenfilenames(initialdir=currdir + "/../Signal", title="Select file",
+    root.filename = filedialog.askopenfilenames(initialdir=currdir + "/../ECG_Data", title="Select file",
                                                 filetypes=(("txt files", "*.txt"),
                                                            ("all files", "*.*")))
     filenames = list(root.filename)
@@ -128,6 +154,7 @@ def main():
 
     out = os.path.join(signal_dir, saveas)
 
+    # Adds labels to the excel sheet
     wb = Workbook(out)
     sheet1 = wb.add_worksheet('Sheet 1')
     sheet1.set_column(1, 1, 12)
@@ -144,22 +171,27 @@ def main():
     sheet1.write(1, 9, 'rMSSD')
     sheet1.write(2, 9, 'STD of Avg')
     sheet1.write(3, 9, 'Avg of STD')
+    interval_format = wb.add_format({'num_format': '#.00'})  # Formatting for the data
+    date_format = wb.add_format({'num_format': 'm/d/y'})
+    time_format = wb.add_format({'num_format': 'hh:mm:ss.000'})
+    diff_format = wb.add_format({'num_format': '0.00'})
+    formats = (interval_format, date_format, time_format, diff_format)
 
-    row = 1
+    row = 1  # Current row in the sheet
 
+    # Handles the multiprocessing, and runs multiple instances of the process_file function
     pool = mp.Pool(processes=max(1, multiprocessing.cpu_count() - 2))
     func = partial(process_file, filenames)
     results = pool.map(func, filenames)
-    # pool = mp.Pool()
-    # results = pool.map(process_file, filenames)
 
     pool.close()
-    # pool.terminate()
     pool.join()
 
-    for result in results:
-        row = write_to_excel(result, sheet1, row, wb)
+    for result in results:  # At this point results contains the list of tuples for each file, and we write
+        # that data into the sheet.
+        row = write_to_excel(result, sheet1, row, formats)
 
+    # Add formulas to calculate more useful data
     sheet1.write_formula('K1', '=STDEV(F:F)')
     sheet1.write_formula('K2', '=SQRT(AVERAGE(G:G))')
     sheet1.write_formula('K3', '=STDEV(H:H)')
@@ -172,6 +204,7 @@ def main():
 
 
 if __name__ == "__main__":
+    # More handling of multiprocessing
     mp.freeze_support()
     main()
     input("Press enter to continue")
